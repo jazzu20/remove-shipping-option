e7132bcb49cf6981513170a51f8b7149e635accd
jazzu20 <arvin.65093@gmail.com>
2018-09-07 16:52:24 +0800

    Update remove-shipping.js

--- remove-shipping.js
+++ remove-shipping.js
@@ -6,7 +6,7 @@ if(steps === "Payment method - ellanacosmetics-staging - Checkout") {
 var str = document.querySelector('.review-block .review-block__content').innerText;
 var a = str.search("Store Pickup");
 
-if(a === "Store Pickup") {
+if(a === -1) {
 document.querySelector('[data-select-gateway="7732396121"]').style.display = "none";
 }
 }
